d4e3a4666c31d76eb253daf0fddc64e6cac553aa
Prachi Indurkar <127858057+prachii007@users.noreply.github.com>
2023-11-20 02:59:00 +0530

    remove comments

--- src/components/home.js
+++ src/components/home.js
@@ -20,9 +20,7 @@ export const Home = () => {
   return (
     <div className='Container'>
       <div className='top-con'>
-        {/* <div style={{ display: 'flex',border: "5px solid red", padding: "17px", justifyContent: 'flex-end', marginBottom: "20px" }}> */}
-        <img className='women' src={Woman} />
-        {/* </div> */}
+        <img className='woman' src={Woman} />
         <div>
           <img src={Logo} className='logo' />
         </div>
@@ -33,11 +31,8 @@ export const Home = () => {
             backgroundColor: "#1A2C2C", width: "90%", color: "#FFFFFFB3", lineHeight: "45px", borderRadius: "20px"
           }}>Employee Productivity Dashboard</div>
           <div style={{
-            // opacity: "0.4",eq
             backgroundColor: "rgb(94, 94, 94,.4)",
             boxShadow: "inset 0px 4px 0px #00000040",
-            // background: "#5E5E5E82 0% 0% no-repeat padding-box ",
-            // border: "1px solid green",
             width: "90%"
             , borderRadius: "10px",
             color: "White",

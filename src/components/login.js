79f424a529ee84798cc0a23ddfaf44523f7e695b
Prachi Indurkar <127858057+prachii007@users.noreply.github.com>
2023-11-19 20:29:04 +0530

    design login page

--- src/components/login.js
+++ src/components/login.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import Cycle from '../assets/Group 3.png'
 
 export const Login = () => {
     return (
-        <div>
-            Login
+        <div className='Container'>
+            <img src={Cycle} style={{ marginBottom: "20px" }} />
+            <div
+                style={{ color: "#36A546CC", fontSize: "16px", marginBottom: "40px" }}>
+                We are electric
+            </div>
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+            }}>
+                <input type='email' placeholder='E-mail' style={{ borderRadius: "20px", lineHeight: "42px", backgroundColor: '#1F1919', boxShadow: "inset 0px 3px 3px #FFFDFD40, 3px 3px 3px #4A494947", border: "black", marginBottom: "25px" }} />
+                <input type='password' placeholder='Password' style={{ borderRadius: "20px", lineHeight: "42px", backgroundColor: '#1F1919', boxShadow: "inset 0px 3px 3px #FFFDFD40, 3px 3px 3px #4A494947", border: 'black', marginBottom: "25px" }} />
+                <button style={{
+                    borderRadius: "20px",
+                    lineHeight: "42px",
+                    color: 'white',
+                    background: 'linear-gradient(#00FF2580,#36A54680, black)',
+                    fontWeight: 'bold',
+                    margin: "30px 0 25px"
+
+                }}>Login</button>
+                <div style={{
+                    fontSize: "15px",
+                    color: "#36A546"
+                }}>Forgot Password?</div>
+            </div>
         </div>
     )
 }

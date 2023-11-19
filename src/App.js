72f25f5c89a3189b9468cdf88855fb8cb323da98
Prachi Indurkar <127858057+prachii007@users.noreply.github.com>
2023-11-19 19:30:47 +0530

    create pages required and route them

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Home } from './components/home';
+import { Login } from './components/login';
+import { Employees } from './components/employees';
 
 function App() {
   return (
     <div className="App">
-   
+      <HashRouter>
+        <Routes>
+          <Route exact path='/' element={<Login />} />
+          <Route exact path='/home' element={<Home />} />
+          <Route exact path='/employees' element={<Employees />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }

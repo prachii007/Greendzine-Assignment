5d39fd94c98f7b5394adeef5eb1a40f5784137d9
Prachi Indurkar <127858057+prachii007@users.noreply.github.com>
2023-11-20 02:42:28 +0530

    create employee card component

---
+++ src/components/empCard.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+export const EmpCard = ({ data }) => {
+    return (
+        <div
+            style={{
+                color: "white",
+                textAlign: 'left',
+                borderRadius: "10px",
+                width: "90%",
+                marginBottom: "20px",
+                padding: "10px",
+                backgroundColor: '#5E5E5E82',
+            }}>
+            <table cellPadding={8}>
+                <tr>
+                    <td>EMP ID</td>
+                    <td>:</td>
+                    <td style={{ fontWeight: "bold" }}>{data.empID}</td>
+                    <td style={{
+                        borderRadius: "50%",
+                        border: " 1px solid black",
+                        height: "20px",
+                        width: "20px",
+                        textAlign: 'center'
+                    }}>{data.empID}</td>
+                </tr>
+                <tr>
+                    <td>Name</td>
+                    <td>:</td>
+                    <td style={{ fontWeight: "bold" }}>{data.name}</td>
+                </tr>
+                <tr>
+                    <td>DOB</td>
+                    <td>:</td>
+                    <td style={{ color: "orange", fontWeight: "bold" }}>{data.dob}</td>
+                </tr>
+                <tr>
+                    <td>Role</td>
+                    <td>:</td>
+                    <td style={{ color: "green", fontWeight: 'bold' }}>{data.role}</td>
+                </tr>
+            </table>
+        </div>
+    )
+}

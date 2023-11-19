6561c2b029ec84adc6711eec41f61891a51125f8
Prachi Indurkar <127858057+prachii007@users.noreply.github.com>
2023-11-19 20:43:32 +0530

    add authentication for login credentials

--- src/components/login.js
+++ src/components/login.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Cycle from '../assets/Group 3.png'
+import { useNavigate } from 'react-router-dom';
 
 export const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
+    const navigate = useNavigate()
+    const goLogin = () => {
+        if (email === "prachi@gmail.com" && password === "password") {
+            navigate('/home')
+        }
+        else {
+            alert("Email or password is incorrect")
+        }
+    }
     return (
         <div className='Container'>
             <img src={Cycle} style={{ marginBottom: "20px" }} />
@@ -13,8 +25,8 @@ export const Login = () => {
                 display: "flex",
                 flexDirection: "column",
             }}>
-                <input type='email' placeholder='E-mail' style={{ borderRadius: "20px", lineHeight: "42px", backgroundColor: '#1F1919', boxShadow: "inset 0px 3px 3px #FFFDFD40, 3px 3px 3px #4A494947", border: "black", marginBottom: "25px" }} />
-                <input type='password' placeholder='Password' style={{ borderRadius: "20px", lineHeight: "42px", backgroundColor: '#1F1919', boxShadow: "inset 0px 3px 3px #FFFDFD40, 3px 3px 3px #4A494947", border: 'black', marginBottom: "25px" }} />
+                <input onChange={obj => setEmail(obj.target.value)} type='email' placeholder='E-mail' style={{ borderRadius: "20px", lineHeight: "42px", color: "white", backgroundColor: '#1F1919', boxShadow: "inset 0px 3px 3px #FFFDFD40, 3px 3px 3px #4A494947", border: "black", marginBottom: "25px" }} />
+                <input onChange={obj => setPassword(obj.target.value)} type='password' placeholder='Password' style={{ borderRadius: "20px", lineHeight: "42px", color: "white", backgroundColor: '#1F1919', boxShadow: "inset 0px 3px 3px #FFFDFD40, 3px 3px 3px #4A494947", border: 'black', marginBottom: "25px" }} />
                 <button style={{
                     borderRadius: "20px",
                     lineHeight: "42px",
@@ -23,7 +35,7 @@ export const Login = () => {
                     fontWeight: 'bold',
                     margin: "30px 0 25px"
 
-                }}>Login</button>
+                }} onClick={goLogin}>Login</button>
                 <div style={{
                     fontSize: "15px",
                     color: "#36A546"

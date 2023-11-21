8e2d49ec666a835feabd9e56ff958e0de26bc51f
Prachi Indurkar <127858057+prachii007@users.noreply.github.com>
2023-11-21 12:34:37 +0530

    change alignment of productivity dashboard text

--- src/components/home.js
+++ src/components/home.js
@@ -31,17 +31,17 @@ export const Home = () => {
             backgroundColor: "#1A2C2C", width: "90%", color: "#8e9085", lineHeight: "45px", borderRadius: "20px"
           }}>Employee Productivity Dashboard</div>
           <div style={{
-            // backgroundColor: "rgb(94, 94, 94,.4)",
             background: "#131917",
             boxShadow: "inset 0px 4px 0px #00000040",
             width: "90%",
             borderRadius: "10px",
             color: "White",
             fontSize: "12px",
-            textAlign: "left"
+            padding: "20px",
+            boxSizing: "border-box"
           }}>
-            <div style={{ margin: "20px 0 " }}>
-              <div style={{ display: "flex", justifyContent: "space-evenly", marginBottom: "10px" }}>
+            <div style={{ marginBottom: "20px" }}>
+              <div style={{ display: "flex", justifyContent: "space-between", width: "80%", margin: "0 auto 10px", marginBottom: "10px" }}>
                 <div>Productivity on Monday</div>
                 <div style={{ color: "#36A546" }}>4%</div>
               </div>
@@ -55,7 +55,7 @@ export const Home = () => {
               }}></div>
             </div>
             <div style={{ marginBottom: "20px" }}>
-              <div style={{ display: "flex", justifyContent: "space-evenly", marginBottom: "10px" }}>
+            <div style={{ display: "flex", justifyContent: "space-between", width: "80%", margin: "0 auto 10px", marginBottom: "10px" }}>
                 <div>Productivity on Tuesday</div>
                 <div style={{ color: "#36A546" }}>92%</div>
               </div>
@@ -69,7 +69,7 @@ export const Home = () => {
               }}></div>
             </div>
             <div style={{ marginBottom: "20px" }}>
-              <div style={{ display: "flex", justifyContent: "space-evenly", marginBottom: "10px" }}>
+            <div style={{ display: "flex", justifyContent: "space-between", width: "80%", margin: "0 auto 10px", marginBottom: "10px" }}>
                 <div>Productivity on Wednesday</div>
                 <div style={{ color: "#36A546" }}>122%</div>
               </div>
@@ -83,7 +83,7 @@ export const Home = () => {
               }}></div>
             </div>
             <div style={{ marginBottom: "20px" }}>
-              <div style={{ display: "flex", justifyContent: "space-evenly", marginBottom: "10px" }}>
+            <div style={{ display: "flex", justifyContent: "space-between", width: "80%", margin: "0 auto 10px", marginBottom: "10px" }}>
                 <div>Productivity on Thursday</div>
                 <div style={{ color: "#36A546" }}>93%</div>
               </div>
@@ -98,7 +98,7 @@ export const Home = () => {
               </div>
             </div>
             <div style={{ marginBottom: "20px" }}>
-              <div style={{ display: "flex", justifyContent: "space-evenly", marginBottom: "10px" }}>
+            <div style={{ display: "flex", justifyContent: "space-between", width: "80%", margin: "0 auto 10px", marginBottom: "10px" }}>
                 <div>Productivity on Friday</div>
                 <div style={{ color: "#36A546" }}>89%</div>
               </div>
@@ -111,7 +111,7 @@ export const Home = () => {
                 marginLeft: "10px"
               }}></div>
             </div> <div style={{ marginBottom: "20px" }}>
-              <div style={{ display: "flex", justifyContent: "space-evenly", marginBottom: "10px" }}>
+            <div style={{ display: "flex", justifyContent: "space-between", width: "80%", margin: "0 auto 10px", marginBottom: "10px" }}>
                 <div>Productivity on Saturday</div>
                 <div style={{ color: "#36A546" }}>98%</div>
               </div>

78af88461e3101f95fd497f35ca05fb620a574e0
Prachi Indurkar <127858057+prachii007@users.noreply.github.com>
2023-11-21 10:25:14 +0530

    enclose td inside tbody

--- src/components/empCard.js
+++ src/components/empCard.js
@@ -13,33 +13,35 @@ export const EmpCard = ({ data }) => {
                 backgroundColor: '#5E5E5E82',
             }}>
             <table cellPadding={8}>
-                <tr>
-                    <td>EMP ID</td>
-                    <td>:</td>
-                    <td style={{ fontWeight: "bold" }}>{data.empID}</td>
-                    <td style={{
-                        borderRadius: "50%",
-                        border: " 1px solid black",
-                        height: "20px",
-                        width: "20px",
-                        textAlign: 'center'
-                    }}>{data.empID}</td>
-                </tr>
-                <tr>
-                    <td>Name</td>
-                    <td>:</td>
-                    <td style={{ fontWeight: "bold" }}>{data.name}</td>
-                </tr>
-                <tr>
-                    <td>DOB</td>
-                    <td>:</td>
-                    <td style={{ color: "orange", fontWeight: "bold" }}>{data.dob}</td>
-                </tr>
-                <tr>
-                    <td>Role</td>
-                    <td>:</td>
-                    <td style={{ color: "green", fontWeight: 'bold' }}>{data.role}</td>
-                </tr>
+                <tbody>
+                    <tr>
+                        <td>EMP ID</td>
+                        <td>:</td>
+                        <td style={{ fontWeight: "bold" }}>{data.empID}</td>
+                        <td style={{
+                            borderRadius: "50%",
+                            border: " 1px solid black",
+                            height: "20px",
+                            width: "20px",
+                            textAlign: 'center'
+                        }}>{data.empID}</td>
+                    </tr>
+                    <tr>
+                        <td>Name</td>
+                        <td>:</td>
+                        <td style={{ fontWeight: "bold" }}>{data.name}</td>
+                    </tr>
+                    <tr>
+                        <td>DOB</td>
+                        <td>:</td>
+                        <td style={{ color: "orange", fontWeight: "bold" }}>{data.dob}</td>
+                    </tr>
+                    <tr>
+                        <td>Role</td>
+                        <td>:</td>
+                        <td style={{ color: "green", fontWeight: 'bold' }}>{data.role}</td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     )
